730a367b3d01dc314cb7b585a9655c8af865b9e2
Jacob Karpovich <jk7674@cs.ship.edu>
2023-02-16 21:05:13 -0500

    ui stuff

--- mobile/src/main/java/com/example/baseballsignaler/MainActivity.java
+++ mobile/src/main/java/com/example/baseballsignaler/MainActivity.java
@@ -1,45 +1,112 @@
 package com.example.baseballsignaler;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.CaptureActivity;
 
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_BLUETOOTH_PERMISSION = 1;
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
 
-    private String macAddress;
+    private TextView resultTextView;
+    @RequiresApi(api = Build.VERSION_CODES.S)
+    private void requestBluetoothPermission(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
 
-    private void connectToWearOSDevice() {
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, REQUEST_BLUETOOTH_PERMISSION);
         }
-        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-        for (BluetoothDevice device : pairedDevices){
-            if(device.getName().startsWith("Wear OS")){
-                macAddress = device.getAddress();
-                break;
-            }
-        }
-        BluetoothDevice wearOSDevice = bluetoothAdapter.getRemoteDevice(macAddress);
-
-
     }
+
+    private void RequestCameraPermission(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA_PERMISSION);
+        }
+    }
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            requestBluetoothPermission();
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Create buttons and textViews to be used with following code */
+        Button buzzButton = findViewById(R.id.buzz_button);
+        Button sendButton = findViewById(R.id.send_button);
+        Button scanButton = findViewById(R.id.scan_button);
+        resultTextView = findViewById(R.id.result_textview);
+
+        /* Create Vibration stuff*/
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        VibrationEffect haptic = VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE);
+        VibrationEffect effect = VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE);
+
+        buzzButton.setOnClickListener(v -> {
+            vibrator.vibrate(haptic);
+            vibrator.vibrate(effect);
+
+            Toast.makeText(this, "Buzz Sent", Toast.LENGTH_SHORT).show();
+        });
+
+        sendButton.setOnClickListener(v -> {
+                vibrator.vibrate(haptic);
+        });
+
+        scanButton.setOnClickListener(v -> {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                RequestCameraPermission();
+            }
+            vibrator.vibrate(haptic);
+
+            IntentIntegrator integrator = new IntentIntegrator(this);
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+            integrator.setPrompt("Scan a QR code");
+            integrator.setBeepEnabled(true);
+            integrator.setOrientationLocked(true);
+            integrator.setCaptureActivity(CaptureActivityPortrait.class);
+            integrator.initiateScan();;
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                resultTextView.setText(R.string.qr_scan_cancelled);
+            } else {
+                Resources res = getResources();
+                resultTextView.setText(String.format(res.getString(R.string.scan_result), result.getContents()));
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
--- wear/src/main/java/com/example/baseballsignaler/MainActivity.java
+++ wear/src/main/java/com/example/baseballsignaler/MainActivity.java
@@ -1,63 +1,87 @@
 package com.example.baseballsignaler;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.example.baseballsignaler.databinding.ActivityMainBinding;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import org.w3c.dom.Text;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.UUID;
 
 public class MainActivity extends Activity {
+    private ImageView qrCodeImageView;
+    private TextView code;
 
-    //private TextView numberTextView;
-    //private ActivityMainBinding binding;
-    private static final UUID UUID = java.util.UUID.fromString("123e4567-e89b-12d3-a456-426614174000");
-    private BluetoothServerSocket serverSocket;
-    private BluetoothSocket socket;
-    private InputStream inputStream;
-
-    private void acceptConnection(){
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        try{
-            serverSocket = bluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord("BaseballCodeSender", UUID);
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
-
-    private String recieveNumberFromPhone(){
-        byte[] buffer = new byte[1024];
-        int bytes;
-        try{
-            bytes = inputStream.read(buffer);
-            return new String(buffer, 0, bytes);
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        return null;
-    }
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //binding = ActivityMainBinding.inflate(getLayoutInflater());
-        //setContentView(binding.getRoot());
-        //numberTextView = binding.numTextView; // numTextView is the id of the element
 
-        acceptConnection();
-        TextView numberDisplay = findViewById(R.id.numTextView);
+        ToggleButton toggleButton = findViewById(R.id.generate_qr_code_button);
+        qrCodeImageView = findViewById(R.id.qr_code_image_view);
+        code = findViewById(R.id.code_display);
 
-        String receivedNumber = recieveNumberFromPhone();
-        numberDisplay.setText(receivedNumber);
-        //int firstCodeNumber = 11; // this should be a passed in var instead of hard code
-        //String formatted = getString(R.string.numToDisplay, firstCodeNumber);
-        //numberTextView.setText(getString(R.string.numToDisplay, firstCodeNumber));
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // Show the QR code
+                    ImageView qrCodeImageView = findViewById(R.id.qr_code_image_view);
+                    qrCodeImageView.setVisibility(View.VISIBLE);
+                    code.setVisibility(View.GONE);
+                    qrCodeImageView.setImageBitmap(generateQrCodeBitmap("testDevice_watch"));
+                } else {
+                    // Hide the QR code
+                    ImageView qrCodeImageView = findViewById(R.id.qr_code_image_view);
+                    qrCodeImageView.setVisibility(View.GONE);
+                    code.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
 
+    private Bitmap generateQrCodeBitmap(String data) {
+        int size = getResources().getDimensionPixelSize(R.dimen.qr_code_size);
+        BitMatrix bitMatrix = null;
+        try {
+            bitMatrix = new QRCodeWriter().encode(data, BarcodeFormat.QR_CODE, size + 50, size + 50);
+        } catch (WriterException e) {
+            e.printStackTrace();
+        }
+        assert bitMatrix != null;
+        int width = bitMatrix.getWidth();
+        int height = bitMatrix.getHeight();
+        int[] pixels = new int[width * height];
+        for (int y = 0; y < height; y++) {
+            int offset = y * width;
+            for (int x = 0; x < width; x++) {
+                pixels[offset + x] = bitMatrix.get(x, y) ? Color.BLACK : Color.WHITE;
+            }
+        }
+        Bitmap qrCodeBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        qrCodeBitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+        return qrCodeBitmap;
     }
 }
+
+
